e78d627705e7a72c4132c466a47169d87ad38972
Anh Tu Nguyen <154424008+tuna7588@users.noreply.github.com>
2024-04-12 12:28:07 +0000

    First commit

--- test.js
+++ test.js
@@ -3,3 +3,13 @@ test('expected 5 elements', () => {
     let fibonacciArray = fibonacci(5);
     expect(fibonacciArray.length).toBe(5);
 });
+
+test('Last element for input 5 should be 3', () => {
+    let fibonacciArray = fibonacci(5);
+    expect(fibonacciArray[4]).toBe(3);
+});
+
+test('Expected 0 elements for negative input', () => {
+    let fibonacciArray = fibonacci(-5);
+    expect(fibonacciArray.length).toBe(0);
+});
